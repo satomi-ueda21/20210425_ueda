c9bc12dfd1fe9d57de23272b9d0225740e002146
satomi-ueda21 <gionkuri@hotmail.com>
2021-04-25 10:11:02 +0900

    first-commit

---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="top-footer">
+    <div class="footer-bar"></div>
+    <small>Copyright 2019 estra inc. All Rights Reserved.</small>
+  </footer>
+</template>
+
+<style scoped>
+  .footer-bar {
+    height: 5px;
+    line-height: 70px;
+  }
+
+  .top-footer {
+    background: #000;
+    text-align: center;
+  }
+
+  .top-footer small {
+    text-align: center;
+    color: #fff;
+  }
+</style>
